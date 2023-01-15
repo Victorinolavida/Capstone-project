8d558c66ed581bac848163ad744d5cfa1afe7665
Victorinolavida <victorinolavida@ciencias.unam.mx>
2023-01-15 11:39:29 -0600

    test added

--- restaurant/models.py
+++ restaurant/models.py
@@ -15,3 +15,5 @@ class Menu(models.Model):
   title = models.CharField(max_length=255)
   price = models.DecimalField(max_digits=10,decimal_places=2)
   inventory = models.PositiveSmallIntegerField()
+  def __str__(self):
+    return f'{self.title} : {str(self.price)}'
